77d0e82c1b4cfb5f8152616d6d8f2b773a9236c9
Samitagrg <emmalia96@gmail.com>
2025-08-06 17:27:45 +0545

    added elements to the background

--- js/script.js
+++ js/script.js
@@ -6,3 +6,34 @@ const panels = {
     contact: document.getElementById('contact-panel'),
     resume: document.getElementById('resume-panel')
 };
+
+// Character position
+let positionX = window.innerWidth / 2;
+let positionY = 10;
+
+// Movement settings
+const moveSpeed = 20;
+const cornerThreshold = 100;
+
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+         case 'ArrowUp':
+            positionY -= moveSpeed;
+            break;
+        case 'ArrowDown':
+            positionY += moveSpeed;
+            break;
+        case 'ArrowLeft':
+            positionX -= moveSpeed;
+            break;
+        case 'ArrowRight':
+            positionX += moveSpeed;
+            break;
+        }
+        updateCharacterPosition();
+});
+
+function updateCharacterPosition() {
+    character.style.top = positionY + 'px';
+    character.style.left = positionX + 'px';
+}
